3e982f017d939fb8e6f37d3ffa7a8afa33a8259d
Ali B. <dmondark@gmail.com>
2011-06-02 16:40:30 -0700

    The file needs to have actual code in order for the plugin to work! Sorry. Closes #1.

---
+++ extensions/helper/Partial.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace app\extensions\helper;
+
+class Partial extends \lithium\template\Helper {
+  public function __call($method, $args) {
+    $params = empty($args)? array() : $args[0];
+    return $this->_context->view()->render(array('element'=> "{$method}_partial"), $params);
+  }
+}
+
+?>
